5bb2dbdc47f227089c644e9b68e9da4be0fc484c
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2025-06-06 05:40:24 +0200

    Update DeployAlert.tsx

--- app/components/deploy/DeployAlert.tsx
+++ app/components/deploy/DeployAlert.tsx
@@ -1,6 +1,7 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import { classNames } from '~/utils/classNames';
 import type { DeployAlert } from '~/types/actions';
+import { Check, X } from 'lucide-react';
 
 interface DeployAlertProps {
   alert: DeployAlert;
@@ -21,7 +22,7 @@ export default function DeployChatAlert({ alert, clearAlert, postMessage }: Depl
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: -20 }}
         transition={{ duration: 0.3 }}
-        className={`rounded-lg border border-bolt-elements-borderColor bg-bolt-elements-background-depth-2 p-4 mb-2`}
+        className={`rounded-[15px] border border-[#EEE] bg-[#FFF] p-4 mb-2`}
       >
         <div className="flex items-start">
           {/* Icon */}
@@ -35,10 +36,10 @@ export default function DeployChatAlert({ alert, clearAlert, postMessage }: Depl
               className={classNames(
                 'text-xl',
                 type === 'success'
-                  ? 'i-ph:check-circle-duotone text-bolt-elements-icon-success'
+                  ? ' text-bolt-elements-icon-success'
                   : type === 'error'
-                    ? 'i-ph:warning-duotone text-bolt-elements-button-danger-text'
-                    : 'i-ph:info-duotone text-bolt-elements-loader-progress',
+                    ? ' text-bolt-elements-button-danger-text'
+                    : ' text-bolt-elements-loader-progress',
               )}
             ></div>
           </motion.div>
@@ -48,7 +49,7 @@ export default function DeployChatAlert({ alert, clearAlert, postMessage }: Depl
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ delay: 0.1 }}
-              className={`text-sm font-medium text-bolt-elements-textPrimary`}
+              className={`text-2xl font-medium text-[#000]`}
             >
               {title}
             </motion.h3>
@@ -70,20 +71,25 @@ export default function DeployChatAlert({ alert, clearAlert, postMessage }: Depl
                         className={classNames(
                           'w-6 h-6 rounded-full flex items-center justify-center',
                           buildStatus === 'running'
-                            ? 'bg-bolt-elements-loader-progress'
+                            ? 'bg-[#FAFAFA] '
                             : buildStatus === 'complete'
-                              ? 'bg-bolt-elements-icon-success'
+                              ? 'bg-black'
                               : buildStatus === 'failed'
-                                ? 'bg-bolt-elements-button-danger-background'
+                                ? 'bg-[#e2dbee]'
                                 : 'bg-bolt-elements-textTertiary',
                         )}
                       >
                         {buildStatus === 'running' ? (
-                          <div className="i-svg-spinners:90-ring-with-bg text-white text-xs"></div>
+                          <div className="">
+                          <svg aria-hidden="true" className="w-[16px] h-[16px] text-gray-200 animate-spin dark:text-gray-600 fill-black" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+  </svg>
+                        </div>
                         ) : buildStatus === 'complete' ? (
-                          <div className="i-ph:check text-white text-xs"></div>
+                          <div><Check size={15} color='#E4E4E4'></Check></div>
                         ) : buildStatus === 'failed' ? (
-                          <div className="i-ph:x text-white text-xs"></div>
+                          <div><X size={15} color='#000'></X></div>
                         ) : (
                           <span className="text-white text-xs">1</span>
                         )}
@@ -95,7 +101,7 @@ export default function DeployChatAlert({ alert, clearAlert, postMessage }: Depl
                     <div
                       className={classNames(
                         'h-0.5 w-8',
-                        buildStatus === 'complete' ? 'bg-bolt-elements-icon-success' : 'bg-bolt-elements-textTertiary',
+                        buildStatus === 'complete' ? 'bg-[#888]' : 'bg-[#888]',
                       )}
                     ></div>
 
@@ -105,20 +111,25 @@ export default function DeployChatAlert({ alert, clearAlert, postMessage }: Depl
                         className={classNames(
                           'w-6 h-6 rounded-full flex items-center justify-center',
                           deployStatus === 'running'
-                            ? 'bg-bolt-elements-loader-progress'
+                            ? 'bg-[#FAFAFA]'
                             : deployStatus === 'complete'
-                              ? 'bg-bolt-elements-icon-success'
+                              ? 'bg-black'
                               : deployStatus === 'failed'
                                 ? 'bg-bolt-elements-button-danger-background'
                                 : 'bg-bolt-elements-textTertiary',
                         )}
                       >
                         {deployStatus === 'running' ? (
-                          <div className="i-svg-spinners:90-ring-with-bg text-white text-xs"></div>
+                          <div className="">
+                            <svg aria-hidden="true" className="w-[16px] h-[16px] text-gray-200 animate-spin dark:text-gray-600 fill-black" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+                          </div>
                         ) : deployStatus === 'complete' ? (
-                          <div className="i-ph:check text-white text-xs"></div>
+                          <div><Check size={15} color='#E4E4E4'></Check></div>
                         ) : deployStatus === 'failed' ? (
-                          <div className="i-ph:x text-white text-xs"></div>
+                          <div><X size={15} color='#000'></X></div>
                         ) : (
                           <span className="text-white text-xs">2</span>
                         )}
@@ -140,10 +151,13 @@ export default function DeployChatAlert({ alert, clearAlert, postMessage }: Depl
                     href={url}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-bolt-elements-item-contentAccent hover:underline flex items-center"
+                    className="text-black flex gap-1 underline flex items-center"
                   >
-                    <span className="mr-1">View deployed site</span>
-                    <div className="i-ph:arrow-square-out"></div>
+                    <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className='h-[18px] w-[18px]' viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1 16.057v-3.057h2.994c-.059 1.143-.212 2.24-.456 3.279-.823-.12-1.674-.188-2.538-.222zm1.957 2.162c-.499 1.33-1.159 2.497-1.957 3.456v-3.62c.666.028 1.319.081 1.957.164zm-1.957-7.219v-3.015c.868-.034 1.721-.103 2.548-.224.238 1.027.389 2.111.446 3.239h-2.994zm0-5.014v-3.661c.806.969 1.471 2.15 1.971 3.496-.642.084-1.3.137-1.971.165zm2.703-3.267c1.237.496 2.354 1.228 3.29 2.146-.642.234-1.311.442-2.019.607-.344-.992-.775-1.91-1.271-2.753zm-7.241 13.56c-.244-1.039-.398-2.136-.456-3.279h2.994v3.057c-.865.034-1.714.102-2.538.222zm2.538 1.776v3.62c-.798-.959-1.458-2.126-1.957-3.456.638-.083 1.291-.136 1.957-.164zm-2.994-7.055c.057-1.128.207-2.212.446-3.239.827.121 1.68.19 2.548.224v3.015h-2.994zm1.024-5.179c.5-1.346 1.165-2.527 1.97-3.496v3.661c-.671-.028-1.329-.081-1.97-.165zm-2.005-.35c-.708-.165-1.377-.373-2.018-.607.937-.918 2.053-1.65 3.29-2.146-.496.844-.927 1.762-1.272 2.753zm-.549 1.918c-.264 1.151-.434 2.36-.492 3.611h-3.933c.165-1.658.739-3.197 1.617-4.518.88.361 1.816.67 2.808.907zm.009 9.262c-.988.236-1.92.542-2.797.9-.89-1.328-1.471-2.879-1.637-4.551h3.934c.058 1.265.231 2.488.5 3.651zm.553 1.917c.342.976.768 1.881 1.257 2.712-1.223-.49-2.326-1.211-3.256-2.115.636-.229 1.299-.435 1.999-.597zm9.924 0c.7.163 1.362.367 1.999.597-.931.903-2.034 1.625-3.257 2.116.489-.832.915-1.737 1.258-2.713zm.553-1.917c.27-1.163.442-2.386.501-3.651h3.934c-.167 1.672-.748 3.223-1.638 4.551-.877-.358-1.81-.664-2.797-.9zm.501-5.651c-.058-1.251-.229-2.46-.492-3.611.992-.237 1.929-.546 2.809-.907.877 1.321 1.451 2.86 1.616 4.518h-3.933z"/></svg>
+                    </div>
+                    <span className="mr-1 relative -top-[1px]">View deployed site</span>
+                    
                   </a>
                 </div>
               )}
@@ -171,18 +185,18 @@ export default function DeployChatAlert({ alert, clearAlert, postMessage }: Depl
                       'flex items-center gap-1.5',
                     )}
                   >
-                    <div className="i-ph:chat-circle-duotone"></div>
-                    Ask Bolt
+                    
+                    Fixing
                   </button>
                 )}
                 <button
                   onClick={clearAlert}
                   className={classNames(
-                    `px-2 py-1.5 rounded-md text-sm font-medium`,
-                    'bg-bolt-elements-button-secondary-background',
-                    'hover:bg-bolt-elements-button-secondary-backgroundHover',
+                    `px-2 py-1.5 h-[35px] w-full rounded-[12px] text-sm font-medium`,
+                    'bg-black',
+                    'hover:bg-[#222] transition-all',
                     'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-bolt-elements-button-secondary-background',
-                    'text-bolt-elements-button-secondary-text',
+                    'text-[#E4E4E4]',
                   )}
                 >
                   Dismiss
