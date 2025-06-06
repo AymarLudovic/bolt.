5a05501a3beee4f2578b12a22f4a0ee108cc7b9e
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2025-06-06 05:25:41 +0200

    Update ExpoQrModal.tsx

--- app/components/workbench/ExpoQrModal.tsx
+++ app/components/workbench/ExpoQrModal.tsx
@@ -13,24 +13,26 @@ export const ExpoQrModal: React.FC<ExpoQrModalProps> = ({ open, onClose }) => {
   const expoUrl = useStore(expoUrlAtom);
 
   return (
-    <DialogRoot open={open} onOpenChange={(v) => !v && onClose()}>
+    <DialogRoot open={open} onOpenChange={(v) => !v && onClose()} >
       <Dialog
         className="text-center !flex-col !mx-auto !text-center !max-w-md"
+        
         showCloseButton={true}
         onClose={onClose}
       >
-        <div className="border !border-bolt-elements-borderColor flex flex-col gap-5 justify-center items-center p-6 bg-bolt-elements-background-depth-2 rounded-md">
-          <div className="i-bolt:expo-brand h-10 w-full invert dark:invert-none"></div>
-          <DialogTitle className="text-bolt-elements-textTertiary text-lg font-semibold leading-6">
+        <div style={{fontFamily:'Funnel Display'}} className="border !border-bolt-elements-borderColor flex flex-col gap-5 justify-center items-center p-6 bg-white rounded-md">
+          <div className="i-bolt:expo h-10 w-full invert dark:invert-none"></div>
+          <DialogTitle className=" text-lg font-semibold leading-6">
             Preview on your own mobile device
           </DialogTitle>
-          <DialogDescription className="bg-bolt-elements-background-depth-3 max-w-sm rounded-md p-1 border border-bolt-elements-borderColor">
+          <DialogDescription className="bg-[#fafafa] max-w-sm rounded-[12px] p-1  border-bolt-elements-borderColor">
             Scan this QR code with the Expo Go app on your mobile device to open your project.
           </DialogDescription>
           <div className="my-6 flex flex-col items-center">
             {expoUrl ? (
               <QRCode
-                logoImage="/favicon.svg"
+                logoImage="/icon.svg"
+
                 removeQrCodeBehindLogo={true}
                 logoPadding={3}
                 logoHeight={50}
@@ -39,7 +41,7 @@ export const ExpoQrModal: React.FC<ExpoQrModalProps> = ({ open, onClose }) => {
                 style={{
                   borderRadius: 16,
                   padding: 2,
-                  backgroundColor: '#8a5fff',
+                  
                 }}
                 value={expoUrl}
                 size={200}
