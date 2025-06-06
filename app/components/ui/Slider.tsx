8e99ce00407aac66614a6f7d9b936efcf8754d27
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2025-06-06 05:49:08 +0200

    Update Slider.tsx

--- app/components/ui/Slider.tsx
+++ app/components/ui/Slider.tsx
@@ -22,16 +22,16 @@ export const Slider = genericMemo(<T,>({ selected, options, setSelected }: Slide
   const isMiddleSelected = hasMiddle && options.middle ? selected === options.middle.value : false;
 
   return (
-    <div className="flex items-center flex-wrap shrink-0 gap-1 bg-bolt-elements-background-depth-1 overflow-hidden rounded-full p-1">
+    <div className="flex items-center flex-wrap shrink-0 gap-1  overflow-hidden rounded-full p-1">
       <SliderButton selected={isLeftSelected} setSelected={() => setSelected?.(options.left.value)}>
         {options.left.text}
       </SliderButton>
 
-      {options.middle && (
+      {/* {options.middle && (
         <SliderButton selected={isMiddleSelected} setSelected={() => setSelected?.(options.middle!.value)}>
           {options.middle.text}
         </SliderButton>
-      )}
+      )} */}
 
       <SliderButton
         selected={!isLeftSelected && !isMiddleSelected}
@@ -54,13 +54,13 @@ const SliderButton = memo(({ selected, children, setSelected }: SliderButtonProp
     <button
       onClick={setSelected}
       className={classNames(
-        'bg-transparent text-sm px-2.5 py-0.5 rounded-full relative',
+        'bg-transparent text-sm px-2.5 py-1 rounded-full relative',
         selected
-          ? 'text-bolt-elements-item-contentAccent'
+          ? 'text-[#E4E4E44] bg-[#000]'
           : 'text-bolt-elements-item-contentDefault hover:text-bolt-elements-item-contentActive',
       )}
     >
-      <span className="relative z-10">{children}</span>
+      <span className="relative z-10 -top-[1px]">{children}</span>
       {selected && (
         <motion.span
           layoutId="pill-tab"
