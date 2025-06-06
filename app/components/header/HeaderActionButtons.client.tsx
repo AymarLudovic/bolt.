020fdca5fa4add6c7a2c7334ebfe23291dc05ef8
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2025-06-06 05:43:21 +0200

    Update HeaderActionButtons.client.tsx

--- app/components/header/HeaderActionButtons.client.tsx
+++ app/components/header/HeaderActionButtons.client.tsx
@@ -1,28 +1,197 @@
 import { useStore } from '@nanostores/react';
+import useViewport from '~/lib/hooks';
+import { chatStore } from '~/lib/stores/chat';
+import { netlifyConnection } from '~/lib/stores/netlify';
+import { vercelConnection } from '~/lib/stores/vercel';
 import { workbenchStore } from '~/lib/stores/workbench';
-import { useState } from 'react';
+import { classNames } from '~/utils/classNames';
+import { useEffect, useRef, useState } from 'react';
 import { streamingState } from '~/lib/stores/streaming';
-import { ExportChatButton } from '~/components/chat/chatExportAndImport/ExportChatButton';
-import { useChatHistory } from '~/lib/persistence';
-import { DeployButton } from '~/components/deploy/DeployButton';
+import { NetlifyDeploymentLink } from '~/components/chat/NetlifyDeploymentLink.client';
+import { VercelDeploymentLink } from '~/components/chat/VercelDeploymentLink.client';
+import { useVercelDeploy } from '~/components/deploy/VercelDeploy.client';
+import { useNetlifyDeploy } from '~/components/deploy/NetlifyDeploy.client';
+import { SiNetlify, SiVercel } from 'react-icons/si';
 
-interface HeaderActionButtonsProps {
-  chatStarted: boolean;
-}
+interface HeaderActionButtonsProps {}
 
-export function HeaderActionButtons({ chatStarted }: HeaderActionButtonsProps) {
+export function HeaderActionButtons({}: HeaderActionButtonsProps) {
+  const showWorkbench = useStore(workbenchStore.showWorkbench);
+  const { showChat } = useStore(chatStore);
+  const netlifyConn = useStore(netlifyConnection);
+  const vercelConn = useStore(vercelConnection);
   const [activePreviewIndex] = useState(0);
   const previews = useStore(workbenchStore.previews);
   const activePreview = previews[activePreviewIndex];
+  const [isDeploying, setIsDeploying] = useState(false);
+  const [deployingTo, setDeployingTo] = useState<'netlify' | 'vercel' | null>(null);
+  const isSmallViewport = useViewport(1024);
+  const canHideChat = showWorkbench || !showChat;
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
   const isStreaming = useStore(streamingState);
-  const { exportChat } = useChatHistory();
+  const { handleVercelDeploy } = useVercelDeploy();
+  const { handleNetlifyDeploy } = useNetlifyDeploy();
 
-  const shouldShowButtons = !isStreaming && activePreview;
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsDropdownOpen(false);
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
+  const onVercelDeploy = async () => {
+    setIsDeploying(true);
+    setDeployingTo('vercel');
+
+    try {
+      await handleVercelDeploy();
+    } finally {
+      setIsDeploying(false);
+      setDeployingTo(null);
+    }
+  };
+
+  const onNetlifyDeploy = async () => {
+    setIsDeploying(true);
+    setDeployingTo('netlify');
+
+    try {
+      await handleNetlifyDeploy();
+    } finally {
+      setIsDeploying(false);
+      setDeployingTo(null);
+    }
+  };
 
   return (
-    <div className="flex items-center">
-      {chatStarted && shouldShowButtons && <ExportChatButton exportChat={exportChat} />}
-      {shouldShowButtons && <DeployButton />}
+    <div className="flex">
+      <div className="relative" ref={dropdownRef}>
+
+        <div style={{border: "1px solid #000"}} className={classNames('w-[150px]  h-[37px] justify-center flex bg-black text-[#E4E4E4] items-center gap-2 py-1 px-2 border border-[#888] rounded-[25px]' ,isDeploying || !activePreview || isStreaming ? 'opacity-[0.9] pointers-events-none': 'opacity-[1] cursor-pointer')}>
+          <Button
+            active
+            disabled={isDeploying || !activePreview || isStreaming}
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            className={classNames('px-4 text-white w-full rounded-[13px] flex items-center justify-center gap-2' ,isDeploying || !activePreview || isStreaming ? 'opacity-[0.8] pointers-events-none': 'opacity-[1] cursor-pointer')}
+          >
+            {isDeploying ? `Deploying...` : 'Deploy'}
+            {/* <div
+              className={classNames('i-ph:caret-down w-4 h-4 transition-transform', isDropdownOpen ? 'rotate-180' : '')}
+            /> */}
+          </Button>
+        </div>
+
+        {isDropdownOpen && (
+          <div className="absolute right-2 flex flex-col gap-1 z-50 p-1 mt-1 min-w-[15.5rem] bg-[#FFF] rounded-[12px] bg-[#FFF] border border-[#EEE]">
+            <Button
+              active
+              onClick={() => {
+                onNetlifyDeploy();
+                setIsDropdownOpen(false);
+              }}
+              disabled={isDeploying || !activePreview || !netlifyConn.user}
+              className="flex items-center w-full px-4 py-2 text-sm text-bolt-elements-textPrimary hover:bg-bolt-elements-item-backgroundActive gap-2 rounded-md group relative"
+            >
+              
+              <SiNetlify size={28}></SiNetlify>
+              <span className="mx-auto">
+                {!netlifyConn.user ? 'Connect your account' : 'Deploy to Netlify'}
+              </span>
+              {netlifyConn.user && <NetlifyDeploymentLink />}
+            </Button>
+            <Button
+              active
+              onClick={() => {
+                onVercelDeploy();
+                setIsDropdownOpen(false);
+              }}
+              disabled={isDeploying || !activePreview || !vercelConn.user}
+              className="flex items-center w-full px-4 py-2 text-sm text-bolt-elements-textPrimary hover:bg-bolt-elements-item-backgroundActive gap-2 rounded-md group relative"
+            >
+              
+              <SiVercel size={22}></SiVercel>
+              <span className="mx-auto">{!vercelConn.user ? 'Connect your account' : 'Deploy to Vercel'}</span>
+              {vercelConn.user && <VercelDeploymentLink />}
+            </Button>
+            {/* <Button
+              active={false}
+              disabled
+              className="flex items-center w-full rounded-md px-4 py-2 text-sm text-bolt-elements-textTertiary gap-2"
+            >
+              <span className="sr-only">Coming Soon</span>
+              <img
+                className="w-5 h-5"
+                height="24"
+                width="24"
+                crossOrigin="anonymous"
+                src="https://cdn.simpleicons.org/cloudflare"
+                alt="cloudflare"
+              />
+              <span className="mx-auto">Deploy to Cloudflare (Coming Soon)</span>
+            </Button> */}
+          </div>
+        )}
+      </div>
+      <div className="flex border md:hidden not-md:flex border-bolt-elements-borderColor rounded-md overflow-hidden">
+        <Button
+          active={showChat}
+          disabled={!canHideChat || isSmallViewport} // expand button is disabled on mobile as it's not needed
+          onClick={() => {
+            if (canHideChat) {
+              chatStore.setKey('showChat', !showChat);
+            }
+          }}
+        >
+          <div className="i-bolt:chat text-sm" />
+        </Button>
+        <div className="w-[1px] bg-bolt-elements-borderColor" />
+        <Button
+          active={showWorkbench}
+          onClick={() => {
+            if (showWorkbench && !showChat) {
+              chatStore.setKey('showChat', true);
+            }
+
+            workbenchStore.showWorkbench.set(!showWorkbench);
+          }}
+        >
+          <div className="i-ph:code-bold" />
+        </Button>
+      </div>
     </div>
+  );
+}
+
+interface ButtonProps {
+  active?: boolean;
+  disabled?: boolean;
+  children?: any;
+  onClick?: VoidFunction;
+  className?: string;
+}
+
+function Button({ active = false, disabled = false, children, onClick, className }: ButtonProps) {
+  return (
+    <button
+      className={classNames(
+        'flex items-center p-1.5',
+        {
+          'bg-bolt-elements-item-backgroundDefault hover:bg-bolt-elements-item-backgroundActive text-bolt-elements-textTertiary hover:text-bolt-elements-textPrimary':
+            !active,
+          'bg-bolt-elements-item-backgroundAccent text-bolt-elements-item-contentAccent': active && !disabled,
+          'bg-bolt-elements-item-backgroundDefault text-alpha-gray-20 dark:text-alpha-white-20 cursor-not-allowed':
+            disabled,
+        },
+        className,
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
